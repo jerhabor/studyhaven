3a39fce9d45d1b973e24eb5dfa0948462df5fd40
Jesse Erhabor <jess.erhabor@gmail.com>
2021-04-16 23:48:31 +0000

    FEAT: Added the products info page and rendered the template to display the required data from  in the database. Navigation links updated accordingly.

--- products/views.py
+++ products/views.py
@@ -1,10 +1,10 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import Product
 
 
 def all_products(request):
     """" View to return a page showing all products
-    and also handle searching and sorting queries """
+    and also handle searching and sorting queries. """
 
     products = Product.objects.all()
     context = {
@@ -12,3 +12,15 @@ def all_products(request):
     }
 
     return render(request, 'products/products.html', context)
+
+
+def product_info(request, product_id):
+    """" View to return a page showing further information
+    of the product that the user has clicked on. """
+
+    product = get_object_or_404(Product, pk=product_id)
+    context = {
+        'product': product,
+    }
+
+    return render(request, 'products/product_info.html', context)
