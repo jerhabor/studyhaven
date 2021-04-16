27f7170ee0013772d1dcf0dbb2538f4d7db750e3
Jesse Erhabor <jess.erhabor@gmail.com>
2021-04-16 22:15:13 +0000

    TEST: Added a test for viewing the page i.e to give a status of 200.

--- products/tests.py
+++ products/tests.py
@@ -1,3 +1,12 @@
 from django.test import TestCase
+from .models import Category, Product
 
-# Create your tests here.
+
+class TestViews(TestCase):
+
+    """ A test that the products page renders a 200 status code"""
+    def test_get_products_page(self):
+        response = self.client.get('/shop/')
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(
+            response, 'products/products.html')
