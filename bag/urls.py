b31284b40600ba80dd7cb13aed2e14558db16cbb
Jesse Erhabor <jess.erhabor@gmail.com>
2021-04-18 21:10:24 +0000

    FEAT: Add_item_to_bag function has been added to the list of views in the bag app. The POST form on the product info template has been created to accommodate this functionality for the user.

--- bag/urls.py
+++ bag/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.view_shopping_bag, name='view_bag')
+    path('', views.view_shopping_bag, name='view_bag'),
+    path('add/<product_id>/', views.add_item_to_bag, name='add_to_bag'),
 ]
--- bag/views.py
+++ bag/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
 
 def view_shopping_bag(request):
@@ -6,3 +6,22 @@ def view_shopping_bag(request):
     user to see the contents that they have chosen to
     add to the bag. """
     return render(request, 'bag/bag.html')
+
+
+def add_item_to_bag(request, product_id):
+    """" View for the user to be able to add a product
+    to their shopping bag. """
+
+    qty = int(request.POST.get('quantity'))
+    redirect_url = request.POST.get('redirect_url')
+
+    shopping_bag = request.session.get('bag', {})
+
+    if product_id in list(shopping_bag.keys()):
+        shopping_bag[product_id] += qty
+    else:
+        shopping_bag[product_id] = qty
+
+    request.session['bag'] = shopping_bag
+    print(request.session['bag'])
+    return redirect(redirect_url)
