662b1c83aaceaf0d7ee34e18e756e8f8b43e4770
Jesse Erhabor <jess.erhabor@gmail.com>
2021-04-18 04:03:12 +0000

    FEAT: Added the sort functionality on the select element on the shop page. The user will be able to select an option to sort and page reloads with relevant settings.

--- products/views.py
+++ products/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render, get_object_or_404, redirect, reverse
 from django.contrib import messages
 # to allow for search criteria in product name OR description
-from django.db.models import Q
+from django.db.models import Q, functions
 from .models import Product, Category
 
 
@@ -17,11 +17,23 @@ def all_products(request):
     method = None
 
     if request.GET:
+        if 'sort' in request.GET:
+            sort_choice = request.GET['sort']
+            sort = sort_choice
+            if sort_choice == 'name':
+                sort_choice = 'name_lower'
+                products = products.annotate(name_lower=functions.Lower('name'))
+            if sort_choice == 'category':
+                sort_choice = 'category__name'
+            if 'method' in request.GET:
+                method = request.GET['method']
+                if method == 'desc':
+                    sort_choice = f'-{sort_choice}'
+            products = products.order_by(sort_choice)
         if 'category' in request.GET:
             categories = request.GET['category'].split(',')
             # search for the 'name' field from the category class in database
             products = products.filter(category__name__in=categories)
-            print(Category.objects)
             categories = Category.objects.filter(name__in=categories)
 
         if 'q' in request.GET:
@@ -31,19 +43,21 @@ def all_products(request):
                 return redirect(reverse('shop'))
 
             # Case-insensitive queries made to search product name/description
-            queries = Q(name__icontains=query) | Q(description__icontains=query)
+            queries = Q(
+                name__icontains=query) | Q(
+                    description__icontains=query)
             products = products.filter(queries)
-        
-        print(categories)
 
+    chosen_sort = f'{sort}_{method}'
     context = {
         'shop': products,
         'chosen_category': categories,
+        'sort': chosen_sort,
         'all_categories': category_list,
         'query': query,
-        'sort_method': method
+        'method': method
     }
-    
+
     return render(request, 'products/products.html', context)
 
 
