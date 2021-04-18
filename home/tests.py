31df9a537d397fdce7ad96bf5786273ba0661327
Jesse Erhabor <jess.erhabor@gmail.com>
2021-04-18 04:04:21 +0000

    TEST: Testing that the home page can be rendered successfully.

--- home/tests.py
+++ home/tests.py
@@ -1,3 +1,11 @@
 from django.test import TestCase
 
-# Create your tests here.
+
+class TestViews(TestCase):
+
+    """ A test that the home page renders a 200 status code"""
+    def test_get_home_page(self):
+        response = self.client.get('/')
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(
+            response, 'home/index.html')
