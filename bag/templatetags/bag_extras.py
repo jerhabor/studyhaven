61da1d4c0bace2dc7435d72ee6a0d6fc2d5ff077
Jesse Erhabor <jess.erhabor@gmail.com>
2021-04-19 21:17:22 +0000

    DOCS: Registered a new filter/tool which is used to calculate the subtotal for each item in the bag depending on their quantity.

---
+++ bag/templatetags/bag_extras.py
@@ -0,0 +1,10 @@
+from django import template
+
+
+# From Django documentation - in order to register filter
+register = template.Library()
+
+# Function to calculate the subtotal in the bag
+@register.filter(name='subtotal_calculation')
+def subtotal_calculation(quantity, price):
+    return quantity * price
