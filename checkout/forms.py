0c0fa605ef6a8ec939dd52d2685e7ae1e17f3e8d
Jesse Erhabor <jess.erhabor@gmail.com>
2021-04-21 18:16:34 +0000

    FEAT/DOCS: Created the form that will be displayed on the template on the checkout page(s).

---
+++ checkout/forms.py
@@ -0,0 +1,44 @@
+from django import forms
+from .models import Order
+
+
+class checkoutOrderForm(forms.ModelForm):
+    class Meta:
+        model = Order
+        field = ('full_name', 'email_address',
+                 'phone_number', 'country', 'address_line1',
+                 'address_line2', 'city_or_town', 'postcode',)
+
+    def __init__(self, *args, **kwargs):
+        # Taken from Code Institute Boutique Ado
+        """
+        Add placeholders and classes, remove auto-generated
+        labels and set autofocus on first field
+        """
+        super().__init__(*args, **kwargs)
+        form_input_placeholders = {
+            'full_name': 'Full Name',
+            'email_address': 'Email Address',
+            'phone_number': 'Phone Number',
+            'country': 'Country',
+            'address_line1': 'Address Line 1',
+            'address_line2': 'Address Line 2',
+            'city_or_town': 'Town/City',
+            'postcode': 'Postal Code',
+        }
+
+        # Cursor to start on Full Name input field when page loads
+        self.fields['full_name'].widget.attrs['autofocus'] = True
+        # Custom CSS Django Styling
+        for field in self.fields:
+            if self.fields[field].required:
+                placeholder = f'{form_input_placeholders[field]} *'
+            else:
+                placeholder = form_input_placeholders[field]
+
+            """ Assigning above styles to the values in the
+            form_input_placeholders dictionary above."""
+            self.fields[field].widget.attrs[
+                'form_input_placeholders'] = placeholder
+            self.fields[field].widget.attrs['class'] = 'stripe-style-input'
+            self.fields[field].label = False
