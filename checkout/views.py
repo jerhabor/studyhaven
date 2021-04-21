a201428c817c5664e593418822c550cbc73df173
Jesse Erhabor <jess.erhabor@gmail.com>
2021-04-21 21:35:43 +0000

    FEAT: View created to handle the rendering of the checkout template. Also includes defensive programing to disable access to checkout if user has not added an item yet.

--- checkout/views.py
+++ checkout/views.py
@@ -1,3 +1,19 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, reverse
+from django.contrib import messages
 
-# Create your views here.
+from .forms import checkoutOrderForm
+
+
+def checkoutOrder(request):
+    bag = request.session.get('bag', {})
+    if not bag:
+        messages.error(request, "You have not added an item to your bag yet!")
+        return redirect(reverse('shop'))
+
+    checkout_order_form = checkoutOrderForm()
+    template = 'checkout/checkout.html'
+    context = {
+        'form': checkout_order_form,
+    }
+
+    return render(request, template, context)
