6171f3c214dea5a955fde06d34034d0bf0d943b9
Jesse Erhabor <jess.erhabor@gmail.com>
2021-04-21 18:15:57 +0000

    DOCS: Overriding the 'ready' method by using the signal.py module that I created. This ensure that the grand_total is being generated correctly using the update_total function in my models.py module.

--- checkout/apps.py
+++ checkout/apps.py
@@ -4,3 +4,7 @@ from django.apps import AppConfig
 class CheckoutConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'checkout'
+
+    # Override the ready method using signals.py
+    def ready(self):
+        import checkout.signals
