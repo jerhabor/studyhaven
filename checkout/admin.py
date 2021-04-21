4daea996fadb05b4518e62a3609905a5e740be9a
Jesse Erhabor <jess.erhabor@gmail.com>
2021-04-21 18:12:44 +0000

    DOCS: Created display in the django admin by setting the fields and determining those that are editable and readonly.

--- checkout/admin.py
+++ checkout/admin.py
@@ -1,3 +1,28 @@
 from django.contrib import admin
+from .models import Order, eachProductOrder
 
-# Register your models here.
+
+class eachProductOrderManagement(admin.TabularInline):
+    model = eachProductOrder
+    readonly_fields = ('product_order_total',)
+
+
+class OrderManagement(admin.ModelAdmin):
+    inlines = (eachProductOrderManagement,)
+
+    fields = ('order_number', 'full_name', 'email_address',
+                'phone_number', 'country', 'address_line1',
+                'address_line2', 'city_or_town', 'postcode',
+                'order_date', 'order_total', 'delivery',
+                'grand_total',)
+
+    readonly_fields = ('order_date', 'order_number', 'delivery',
+                     'order_total', 'grand_total',)
+
+    list_display = ('order_number', 'order_date', 'full_name',
+                       'delivery', 'order_total', 'grand_total',)
+
+    ordering = ('-order_date',)
+
+
+admin.site.register(Order, OrderManagement)
