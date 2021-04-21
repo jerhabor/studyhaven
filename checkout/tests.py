c39fc54995ca8a57a70de35c75d808b5371b9df0
Jesse Erhabor <jess.erhabor@gmail.com>
2021-04-21 21:36:35 +0000

    TEST: Checked that the checkout template renders a 200 status code.

--- checkout/tests.py
+++ checkout/tests.py
@@ -1,3 +1,11 @@
 from django.test import TestCase
 
-# Create your tests here.
+
+class TestViews(TestCase):
+
+    """ A test that the checkout page renders a 200 status code"""
+    def test_get_checkout_page(self):
+        response = self.client.get('/checkout/')
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(
+            response, 'checkout/checkout.html')
