a61f560d8ea98e1717810a947f0044003045ede6
Jesse Erhabor <jess.erhabor@gmail.com>
2021-04-21 01:31:24 +0000

    DOCS/STYLE: Added functionality to allow success toast messages to display when adding to bag, updating bag items and deleting items or error if user has not entered a search criteria.

--- bag/views.py
+++ bag/views.py
@@ -1,20 +1,26 @@
 from django.contrib import messages
-from django.shortcuts import render, redirect, HttpResponse, reverse
+from django.shortcuts import render, redirect, HttpResponse, reverse, get_object_or_404
 from products.models import Product
+
+class color:
+   BOLD = '\033[1m'
+   UNDERLINE = '\033[4m'
+   END = '\033[0m'
 
 
 def view_shopping_bag(request):
-    """" View to return the shopping bag page for the
+    """ View to return the shopping bag page for the
     user to see the contents that they have chosen to
     add to the bag. """
     return render(request, 'bag/bag.html')
 
 
 def add_item_to_bag(request, product_id):
-    """" View for the user to be able to add a product
+    """ View for the user to be able to add a product
     to their shopping bag. """
 
-    shop_product = Product.objects.get(pk=product_id)
+    # incase object isn't found
+    shop_product = get_object_or_404(Product, pk=product_id)
     qty = int(request.POST.get('quantity'))
     redirect_url = request.POST.get('redirect_url')
 
@@ -22,26 +28,34 @@ def add_item_to_bag(request, product_id):
 
     if product_id in list(shopping_bag.keys()):
         shopping_bag[product_id] += qty
-        messages.success(request, f'{qty} x {shop_product.name}')
+        messages.success(
+            request, f'Updated {shop_product.name} quantity to {shopping_bag[product_id]}.')
     else:
         shopping_bag[product_id] = qty
-        messages.success(request, f'{qty} x {shop_product.name}')
+        messages.success(
+            request, f'Added {qty} x {shop_product.name} to your bag.')
 
     request.session['bag'] = shopping_bag
     return redirect(redirect_url)
 
 
 def update_bag(request, product_id):
-    """" View for the user to be able to adjust quantity
+    """ View for the user to be able to adjust quantity
     product in the bag and the bag updates accordingly """
 
+    # incase object isn't found
+    shop_product = get_object_or_404(Product, pk=product_id)
     qty = int(request.POST.get('quantity'))
     shopping_bag = request.session.get('bag', {})
 
     if qty > 0:
         shopping_bag[product_id] = qty
+        messages.success(
+            request, f'Updated {shop_product.name} quantity to {shopping_bag[product_id]}.')
     else:
         shopping_bag.pop(product_id)
+        messages.success(
+            request, f'Removed {shop_product.name} from your bag.')
 
     request.session['bag'] = shopping_bag
     return redirect(reverse('view_bag'))
@@ -53,11 +67,16 @@ def remove_bag_item(request, product_id):
     will also update once remove event occurs """
 
     try:
+        # incase object isn't found
+        shop_product = get_object_or_404(Product, pk=product_id)
         shopping_bag = request.session.get('bag', {})
         shopping_bag.pop(product_id)
+        messages.success(
+            request, f'Removed {shop_product.name} from your bag.')
 
         request.session['bag'] = shopping_bag
         return HttpResponse(status=200)
 
     except Exception as e:
+        messages.error(request, f'Unable to remove: {e}')
         return HttpResponse(status=500)
