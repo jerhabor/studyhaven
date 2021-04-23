5fe122d97ba9c1690e1a27e29fe3e85901868264
Jesse Erhabor <jess.erhabor@gmail.com>
2021-04-23 21:58:39 +0000

    DOCS: Added webhook url and subsequently added the webhook on Stripe with the same path of checkout/wh/

--- checkout/urls.py
+++ checkout/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
 from . import views
+from .webhooks import webhook_view
 
 urlpatterns = [
     path('', views.checkout_order, name='checkout'),
     path('checkout_success/<order_number>',
          views.checkout_success, name='checkout_success'),
+    path('wh/', webhook_view, name='webhook'),
 ]
