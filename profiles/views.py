c2ac29cfabfba6855d1e1c5e41e876e3ce50d59c
Jesse Erhabor <jess.erhabor@gmail.com>
2021-04-24 23:23:13 +0000

    DOCS/FEAT: Retrieved the UserProfileForm instance and rendered it to the template to display the crispy form fields.

--- profiles/views.py
+++ profiles/views.py
@@ -1,15 +1,20 @@
 from django.shortcuts import render, get_object_or_404
 
 from .models import UserProfile
+from .forms import UserProfileForm
 
 
 def user_profile(request):
     """ A view to display a registered user's profile """
     profile = get_object_or_404(UserProfile, user=request.user)
 
+    form = UserProfileForm(instance=profile)
+    order_history = profile.order_history.all()
+
     template = 'profiles/profile.html'
     context = {
-        'profile': profile
+        'form': form,
+        'order_history': order_history,
     }
 
     return render(request, template, context)
