21740489b595ab5f88ec50992c526112fa1cff45
Jesse Erhabor <jess.erhabor@gmail.com>
2021-04-24 23:41:50 +0000

    DOCS/FEAT: Added the POST method in the view so the form now submits and saves as the page is reloaded and displays the user's input again.

--- profiles/views.py
+++ profiles/views.py
@@ -1,3 +1,4 @@
+from django.contrib import messages
 from django.shortcuts import render, get_object_or_404
 
 from .models import UserProfile
@@ -8,6 +9,17 @@ def user_profile(request):
     """ A view to display a registered user's profile """
     profile = get_object_or_404(UserProfile, user=request.user)
 
+    # Handling the posting of the form
+    if request.method == 'POST':
+        # Create a new instance of the form into 'profile' defined above
+        form = UserProfileForm(request.POST, instance=profile)
+        # Add success message if form is valid
+        if form.is_valid():
+            form.save()
+            messages.success(
+                request, 'Your contact and shipping information \
+                    have been updated successfully!')
+
     form = UserProfileForm(instance=profile)
     order_history = profile.order_history.all()
 
@@ -15,6 +27,7 @@ def user_profile(request):
     context = {
         'form': form,
         'order_history': order_history,
+        'on_my_profile': True,
     }
 
     return render(request, template, context)
