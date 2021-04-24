8e1f49be111f47ade5dbca68b0bd497796c4e8b7
Jesse Erhabor <jess.erhabor@gmail.com>
2021-04-24 21:29:46 +0000

    DOCS: Rendered out the profile username as a test onto the user profile page. Also tested the registration and login processes.

--- profiles/views.py
+++ profiles/views.py
@@ -1,10 +1,15 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+
+from .models import UserProfile
 
 
 def user_profile(request):
-    """ A view to display the registered user's profile """
+    """ A view to display a registered user's profile """
+    profile = get_object_or_404(UserProfile, user=request.user)
 
     template = 'profiles/profile.html'
-    context = {}
+    context = {
+        'profile': profile
+    }
 
     return render(request, template, context)
