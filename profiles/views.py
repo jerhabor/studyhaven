5819da6d4b786e01311a2758218c83a2c0cabf82
Jesse Erhabor <jess.erhabor@gmail.com>
2021-04-24 18:14:37 +0000

    FEAT: Created the profile template and tested rendering it out with a view and adding it to the StudyHaven list of urls.

--- profiles/views.py
+++ profiles/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
 
-# Create your views here.
+
+def user_profile(request):
+    """ A view to display the registered user's profile """
+
+    template = 'profiles/profile.html'
+    context = {}
+
+    return render(request, template, context)
