5d5e69752efaa8da7455042b36b89d52be8ca00a
Jesse Erhabor <jess.erhabor@gmail.com>
2021-04-27 16:29:09 +0000

    FIX: Changed the : to = for the AWS Bucket Configuration variables. Added the S3 Bucket configurations to allow static files to be uploaded to S3 as project is deployed to Heroku.

--- studyhaven/settings.py
+++ studyhaven/settings.py
@@ -197,11 +197,11 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 if 'USE_AWS' in os.environ:
     # Amazon Web Services Bucket Configuration
-    AWS_STORAGE_BUCKET_NAME: 'studyhaven'
-    AWS_S3_REGION_NAME: 'eu-west-2'
-    AWS_ACCESS_KEY_ID: os.environ.get('AWS_ACCESS_KEY_ID')
-    AWS_SECRET_ACCESS_KEY: os.environ.get('SECRET_ACCESS_KEY')
-    AWS_S3_CUSTOM_DOMAIN: f'{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com'
+    AWS_STORAGE_BUCKET_NAME = 'studyhaven'
+    AWS_S3_REGION_NAME = 'eu-west-2'
+    AWS_ACCESS_KEY_ID = os.environ.get('AWS_ACCESS_KEY_ID')
+    AWS_SECRET_ACCESS_KEY = os.environ.get('SECRET_ACCESS_KEY')
+    AWS_S3_CUSTOM_DOMAIN = f'{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com'
 
     # Storage of Static & Media Files
     STATICFILES_STORAGE = 'custom_storages.StaticFilesStorage'
