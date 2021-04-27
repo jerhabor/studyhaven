ac36765c5440ea812869fe59375703041db058a5
Jesse Erhabor <jess.erhabor@gmail.com>
2021-04-27 16:36:28 +0000

    FEAT: Added cache control to allow to static files to be cached for a long period of time to improve the performance and experience of the site as the user makes multiple visits to the site.

--- studyhaven/settings.py
+++ studyhaven/settings.py
@@ -196,6 +196,12 @@ MEDIA_ROOT = BASE_DIR / 'media'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 if 'USE_AWS' in os.environ:
+    # Allow to Cache static files for a long period of time for better UX
+    AWS_S3_OBJECT_PARAMETERS = {
+        'Expires': 'Thu, 31 Dec 2099 20:00:00 GMT',
+        'CacheControl': 'max-age=94608000',
+    }
+
     # Amazon Web Services Bucket Configuration
     AWS_STORAGE_BUCKET_NAME = 'studyhaven'
     AWS_S3_REGION_NAME = 'eu-west-2'
