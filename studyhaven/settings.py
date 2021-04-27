4a3820742ddf6eaaee68a139c4ccfe54e33d08cb
Jesse Erhabor <jess.erhabor@gmail.com>
2021-04-27 14:27:30 +0000

    DOCS/MAINT: Removed the secret key and replaced it with one in the environment. Also changed the value of DEBUG to only be true if in development mode with the value in the environment but false for deployed version.

--- studyhaven/settings.py
+++ studyhaven/settings.py
@@ -22,10 +22,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-o-4ngup54ckmz2pqfmf7fiv5f3+321_grtab5rfav-29ws5sl$'
+SECRET_KEY = os.environ.get('SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = 'DEVELOPMENT' in os.environ
 
 ALLOWED_HOSTS = ['studyhaven.herokuapp.com', 'localhost']
 
