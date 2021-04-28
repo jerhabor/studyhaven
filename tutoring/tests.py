db7bc3191fe3ca97c46129f761c276a149f826d5
Jesse Erhabor <jess.erhabor@gmail.com>
2021-04-28 02:58:56 +0000

    TEST: Tested that the tutoring page renders successfully and displays a HTTP status response code of 200.

--- tutoring/tests.py
+++ tutoring/tests.py
@@ -1,3 +1,11 @@
 from django.test import TestCase
 
-# Create your tests here.
+
+class TestTutoringViews(TestCase):
+
+    """ A test that the Tutoring page renders a 200 status code"""
+    def test_get_tutoring_page(self):
+        response = self.client.get('/tutoring/')
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(
+            response, 'tutoring/tutoring.html')
