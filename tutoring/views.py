c1957d376d519bf875eed81b4630d6d48800bebc
Jesse Erhabor <jess.erhabor@gmail.com>
2021-04-29 00:46:01 +0000

    FEAT: Rendered the rates data into the tutoring template to make the page more dynamic. Also added in the calendly widget as inline-text so save the user from clicking which requires more effort.

--- tutoring/views.py
+++ tutoring/views.py
@@ -1,6 +1,12 @@
 from django.shortcuts import render
+from .models import TutoringRate
 
 
 def tutoring(request):
-    """" View to returns the tutoring page """
-    return render(request, 'tutoring/tutoring.html')
+    """" View to return the tutoring page """
+    rates = TutoringRate.objects.all()
+    template = 'tutoring/tutoring.html'
+    context = {
+        'rates': rates,
+    }
+    return render(request, template, context)
