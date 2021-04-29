1fe59012ddaa9d6a47fa066a6aaa866a73b41aa6
Jesse Erhabor <jess.erhabor@gmail.com>
2021-04-29 01:52:43 +0000

    FEAT: Added an FAQ model to be generating an accordion item as the superuser enters a new question and answer in the database. This now makes the page more dynamic.

--- faqs/admin.py
+++ faqs/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
+from .models import FAQ
 
-# Register your models here.
+
+class FaqAdmin(admin.ModelAdmin):
+    list_display = (
+        'question',
+    )
+
+    fields = ('question', 'answer',)
+
+
+admin.site.register(FAQ, FaqAdmin)
